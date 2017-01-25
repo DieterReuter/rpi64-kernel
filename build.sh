72cb79d94dc0f83f362cc936152dda8b6d200de1
Dieter Reuter <dieter.reuter@me.com>
2017-01-25 15:55:48 +0100

    Add Docker optimized defconfig

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
+export DEFCONFIG=docker_rpi3_defconfig
 docker-compose build
 docker-compose run builder
--- travis-build.sh
+++ travis-build.sh
@@ -18,7 +18,7 @@ export BUILD_NR="$(date '+%Y%m%d-%H%M%S')"
 echo "BUILD_NR=$BUILD_NR"
 
 # run build
-export DEFCONFIG=
+export DEFCONFIG=docker_rpi3_defconfig
 docker-compose build
 docker-compose run builder
 
