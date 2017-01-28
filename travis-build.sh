df255b4e1fbd7b453fccd3c367878bd244de0c35
Dieter Reuter <dieter.reuter@me.com>
2017-01-28 22:24:30 +0100

    Abort travis build if GITHUB_TOKEN is missing

--- travis-build.sh
+++ travis-build.sh
@@ -5,12 +5,12 @@ set -x
 # This script is meant to run on Travis-CI only
 if [ -z "$TRAVIS_BRANCH" ]; then 
   echo "ABORTING: this script runs on Travis-CI only"
-  exit
+  exit 1
 fi
 # Check essential envs
 if [ -z "$GITHUB_TOKEN" ]; then
   echo "ABORTING: env GITHUB_TOKEN is missing"
-  exit
+  exit 1
 fi
 
 # create a build number
