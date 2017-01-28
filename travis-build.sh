b4996083e906461259ff8139ccb5623e0fbb86af
Dieter Reuter <dieter.reuter@me.com>
2017-01-28 22:52:49 +0100

    Add verbose logging

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-set -x
 
 # This script is meant to run on Travis-CI only
 if [ -z "$TRAVIS_BRANCH" ]; then 
@@ -12,6 +11,9 @@ if [ -z "$GITHUB_TOKEN" ]; then
   echo "ABORTING: env GITHUB_TOKEN is missing"
   exit 1
 fi
+
+# verbose logging
+set -x
 
 # create a build number
 export BUILD_NR="$(date '+%Y%m%d-%H%M%S')"
