857b8119459b37cae5bfd55e2996391cd50ea92e
Dieter Reuter <dieter.reuter@me.com>
2017-03-03 16:19:52 +0100

    Use free Travis CI
    Signed-off-by: Dieter Reuter <dieter.reuter@me.com>

--- travis-build.sh
+++ travis-build.sh
@@ -26,7 +26,7 @@ docker-compose run builder
 
 # deploy to GitHub releases
 export GIT_TAG=v$BUILD_NR
-export GIT_RELTEXT="Auto-released by [Travis-CI build #$TRAVIS_BUILD_NUMBER](https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID)"
+export GIT_RELTEXT="Auto-released by [Travis-CI build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID)"
 curl -sSL https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_linux_amd64.zip > ghr.zip
 unzip ghr.zip
 ./ghr --version
