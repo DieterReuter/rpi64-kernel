83f2084b4b57d1618d76cd03d597746933433c1a
Dieter Reuter <dieter.reuter@me.com>
2018-04-29 16:29:26 +0200

    Use kernel defconfigs for kernel 4.14.y
    Signed-off-by: Dieter Reuter <dieter.reuter@me.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 set -e
 
 export DEFCONFIG=docker_rpi3_defconfig
-export DEFCONFIG=bcmrpi3_defconfig
 docker-compose -p rpikernelbuild build
 docker-compose -p rpikernelbuild run builder
 docker-compose -p rpikernelbuild down
